8d3ecc98ca207e34bfdca8a9eb508af17c9ba490
chengang <cg-0508@outlook.com>
2020-11-23 22:04:18 +0800

    jwt

--- user-service/handler/handler.go
+++ user-service/handler/handler.go
@@ -1,9 +1,11 @@
 package handler
 
 import (
+	"errors"
 	"fmt"
 	pb "github.com/cg-0508/laracom/user-service/proto/user"
 	"github.com/cg-0508/laracom/user-service/repo"
+	"github.com/cg-0508/laracom/user-service/service"
 	"golang.org/x/crypto/bcrypt"
 	"golang.org/x/net/context"
 	"log"
@@ -11,6 +13,7 @@ import (
 
 type UserService struct {
 	Repo repo.Repository
+	Token service.Authable
 }
 
 func (srv *UserService) Get(ctx context.Context, req *pb.User, res *pb.Response) error {
@@ -54,6 +57,48 @@ func (srv *UserService) Create(ctx context.Context, req *pb.User, res *pb.Respon
 
 	res.User = req
 	fmt.Print(666)
+
+	return nil
+}
+
+
+func (srv *UserService) Auth(ctx context.Context, req *pb.User, res *pb.Token) error {
+	log.Println("Logging in with:", req.Email, req.Password)
+	// 获取用户信息
+	user, err := srv.Repo.GetByEmail(req.Email)
+	log.Println(user)
+	if err != nil {
+		return err
+	}
+
+	// 校验用户输入密码是否于数据库存储密码匹配
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(req.Password)); err != nil {
+		return err
+	}
+
+	// 生成 jwt token
+	token, err := srv.Token.Encode(user)
+	if err != nil {
+		return err
+	}
+	res.Token = token
+	return nil
+}
+
+func (srv *UserService) ValidateToken(ctx context.Context, req *pb.Token, res *pb.Token) error {
+
+	// 校验用户亲求中的token信息是否有效
+	claims, err := srv.Token.Decode(req.Token)
+
+	if err != nil {
+		return err
+	}
+
+	if claims.User.Id == "" {
+		return errors.New("无效的用户")
+	}
+
+	res.Valid = true
 
 	return nil
 }
