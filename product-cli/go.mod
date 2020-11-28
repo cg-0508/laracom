8ba2709fd11d9dc32beeb31fe882139e031637c2
chengang <cg-0508@outlook.com>
2020-11-28 20:52:12 +0800

    product-cli

--- product-cli/go.mod
+++ product-cli/go.mod
@@ -3,7 +3,10 @@ module github.com/cg-0508/laracom/product-cli
 go 1.13
 
 require (
+	github.com/cg-0508/laracom/product-service v0.0.0-20201128122739-7f0a0a08b2de
 	github.com/micro/cli/v2 v2.1.2
 	github.com/micro/go-micro/v2 v2.9.1
 	golang.org/x/net v0.0.0-20201110031124-69a78807bb2b
 )
+
+replace google.golang.org/grpc => google.golang.org/grpc v1.26.0
--- product-cli/main.go
+++ product-cli/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/micro/cli/v2"
 	"github.com/micro/go-micro/v2"
-	pb "github.com/cg-0508/laracom/product-service/proto/user"
+	pb "github.com/cg-0508/laracom/product-service/proto/product"
 	"golang.org/x/net/context"
 	"log"
 	"os"
@@ -11,111 +11,34 @@ import (
 
 /**
 go run main.go --registry=mdns
-go run main.go --registry=mdns
  */
 func main()  {
 
 	// 初始化客户端服务，定义命令行参数标识
-	service := micro.NewService(
-		micro.Flags(
-			&cli.StringFlag{
-				Name:  "name",
-				Usage: "Your Name",
-			},
-			&cli.StringFlag{
-				Name:  "email",
-				Usage: "Your Email",
-			},
-			&cli.StringFlag{
-				Name:  "password",
-				Usage: "Your Password",
-			},
-			&cli.StringFlag{
-				Name:  "userId",
-				Usage: "userId",
-			},
-		),
-	)
+	service := micro.NewService()
 
 	// 远程服务客户端调用句柄
-	client := pb.NewUserService("laracom.service.user", service.Client())
+	client := pb.NewProductService("laracom.service.product", service.Client())
 
 	// 运行客户端命令调用远程服务逻辑设置
 	service.Init(
 		micro.Action(func(c *cli.Context) error {
-			name := c.String("name")
-			email := c.String("email")
-			password := c.String("password")
-			userId := c.String("userId")
-
-			log.Println("参数:", name, email, password, userId)
-
-			// 调用用户服务
-			if userId != "" {
-				r, err := client.Update(context.TODO(), &pb.User{
-					Id: userId,
-					Name: name,
-					Email: email,
-					Password: password,
-				})
-				if err != nil {
-					log.Fatalf("更新用户失败: %v", err)
-					return err
-				}
-				log.Printf("更新用户成功: %s", r.User.Id)
-
-			}else{
-				r, err := client.Create(context.TODO(), &pb.User{
-					Name: name,
-					Email: email,
-					Password: password,
-				})
-				if err != nil {
-					log.Fatalf("创建用户失败: %v", err)
-					return err
-				}
-				log.Printf("创建用户成功: %s", r.User.Id)
-				// 调用用户认证服务
-				var token *pb.Token
-				token, err = client.Auth(context.TODO(), &pb.User{
-					Email: email,
-					Password: password,
-				})
-				if err != nil {
-					log.Fatalf("用户登录失败: %v", err)
-				}
-				log.Printf("用户登录成功：%s", token.Token)
-
-				// 调用用户验证服务
-				token, err = client.ValidateToken(context.TODO(), token)
-				if err != nil {
-					log.Fatalf("用户认证失败: %v", err)
-				}
-				log.Printf("用户认证成功：%s", token.Valid)
-
-				getAll, err := client.GetAll(context.Background(), &pb.Request{})
-				if err != nil {
-					log.Fatalf("获取所有用户失败: %v", err)
-					return err
-				}
-				for _, v := range getAll.Users {
-					log.Println(v)
-				}
-
-				// 重置密码
-				resetResp, err := client.CreatePasswordReset(context.Background(), &pb.PasswordReset{
-					Email: email,
-					Token: "password_reset_token",
-				})
-				log.Printf("密码重置token记录：%v", resetResp)
-				// 验证重置token
-				validateResetToken, err := client.ValidatePasswordResetToken(context.Background(), &pb.Token{
-					Token: "password_reset_token",
-				})
-				log.Printf("密码重置token验证结果：%v", validateResetToken.Valid)
-
+			r, err := client.Create(context.TODO(), &pb.Product{
+				Id: 11,
+				BrandId: 11,
+				Sku: "2222",
+				Name: "asdas",
+				Slug: "asdasdas",
+				Description: "asdasdsad",
+				Price: 11,
+				SalePrice: 11,
+				Status: 11,
+			})
+			if err != nil {
+				log.Fatalf("创建商品失败: %v", err)
+				return err
 			}
-
+			log.Printf("创建商品成功: %s", r.Product.Id)
 
 			os.Exit(0)
 			return nil
