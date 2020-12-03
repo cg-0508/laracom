977fd9758941cd70106137facacd688760fdab5e
chengang <cg-0508@outlook.com>
2020-12-03 21:18:08 +0800

    etcd 集群 e3w

--- user-service/db/database.go
+++ user-service/db/database.go
@@ -4,19 +4,20 @@ import (
 	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"os"
 )
 
 func CreateConnection() (*gorm.DB, error) {
 
 	// 从系统环境变量获取数据库信息
-	//host := os.Getenv("DB_HOST")
-	//user := os.Getenv("DB_USER")
-	//DBName := os.Getenv("DB_NAME")
-	//password := os.Getenv("DB_PASSWORD")
-	host := "127.0.0.1:3307"
-	user := "root"
-	DBName := "laracom_user"
-	password := "root"
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	DBName := os.Getenv("DB_NAME")
+	password := os.Getenv("DB_PASSWORD")
+	//host := "127.0.0.1:3307"
+	//user := "root"
+	//DBName := "laracom_user"
+	//password := "root"
 	return gorm.Open(
 		"mysql",
 		fmt.Sprintf(
